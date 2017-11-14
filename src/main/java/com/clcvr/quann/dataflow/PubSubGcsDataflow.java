2552c49dd55f3d90388b78cf226dd023b8bfe319
Vaibhav Kapur <cloudcover-vaibhav@Vaibhavs-MacBook-Air.local>
2017-11-14 17:44:43 +0530

    updation

--- src/main/java/com/clcvr/quann/dataflow/PubSubGcsDataflow.java
+++ src/main/java/com/clcvr/quann/dataflow/PubSubGcsDataflow.java
@@ -3,8 +3,11 @@ package com.clcvr.quann.dataflow;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.zip.GZIPInputStream;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.options.Default;
@@ -16,6 +19,10 @@ import org.apache.beam.sdk.repackaged.org.apache.commons.compress.compressors.bz
 import org.joda.time.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.api.client.util.Charsets;
+import com.google.common.io.ByteSource;
+
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
@@ -24,7 +31,10 @@ import org.apache.beam.sdk.transforms.ParDo;
 
 import org.apache.beam.sdk.transforms.windowing.FixedWindows;	
 import org.apache.beam.sdk.transforms.windowing.Window;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.commons.compress.utils.IOUtils;
 
+import java.util.Base64;
 
 
 /**
@@ -52,15 +62,41 @@ public class PubSubGcsDataflow {
 		 */
 		@Description("Path of the file to write to")
 		@Required
-		@Default.String("gs://log-agg/output/")
+		@Default.String("gs://quann-logs/output/")
 		String getOutput();
 		void setOutput(String value);
 
 		@Description("Fixed window duration, in minutes")
+		@Required
 		@Default.Integer(WINDOW_SIZE)
 		Integer getWindowSize();
 		void setWindowSize(Integer value);
-
+		
+		@Description("Read PubSub Topic")
+		@Required
+		@Default.String("projects/project-lms-182110/subscriptions/apache-nifi")
+		String getReadSubscriptionName();
+		void setReadSubscriptionName(String value);
+		
+		@Description("Read PubSub Topic")
+		@Required
+		@Default.String("projects/project-lms-182110/subscriptions/apache-nifi-netflow")
+		String getNetflowSubscriptionName();
+		void setNetflowSubscriptionName(String value);
+		
+		
+		@Description("Write PubSub Topic")
+		@Required
+		@Default.String("projects/project-lms-182110/topics/logstash")
+		String getWriteTopicName();
+		void setWriteTopicName(String value);
+		
+		@Description("Path of the file to write to")
+		@Required
+		@Default.String("gs://quann-logs/netflow/")
+		String getNetflowOutput();
+		void setNetflowOutput(String value);
+		
 	}
 
 	/**
@@ -93,6 +129,7 @@ public class PubSubGcsDataflow {
 		return outStr;
 		
 	}
+	
 
 	static class LogsFn extends DoFn<PubsubMessage, String> {
 		/**
@@ -102,22 +139,43 @@ public class PubSubGcsDataflow {
 		private static final long serialVersionUID = 1L;
 
 		@ProcessElement
-		public void processElement(ProcessContext c) {
+		public void processElement(ProcessContext c) throws IOException {
 			LOG.info("Found context " + c);
 			// Get the input element from ProcessContext.
 			PubsubMessage message = c.element();
 			// Use ProcessContext.output to emit the output element.
+			
 			String part = "";
-			try {
-				part = PubSubGcsDataflow.decompress(message.getPayload());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			// only for decompression
+			//part = PubSubGcsDataflow.decompress(message.getPayload());
+		
+			part = new String(message.getPayload());
 			c.output(part);
 		}
 	}
+	
+	
+	static class ConvertBinaryToString extends DoFn<PubsubMessage, String> {
+		/**
+		 * This is DoFn function that process one element
+		 * at a time from input PCollection
+		 */
+		private static final long serialVersionUID = 1L;
 
-
+		@ProcessElement
+		public void processElement(ProcessContext c) throws IOException {
+			LOG.info("Found context " + c);
+			// Get the input element from ProcessContext.
+			PubsubMessage message = c.element();
+			// Use ProcessContext.output to emit the output element.
+			System.out.println("Without Conversion"+message);
+			String part = "";
+			part = new String(message.getPayload());
+			System.out.println("After Conversion"+part);
+			c.output(part);
+		}
+	}
+	
 	/**
 	 * @param args
 	 * @throws IOException 
@@ -125,28 +183,39 @@ public class PubSubGcsDataflow {
 	public static void main(String[] args) throws IOException , Exception{
 
 		Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
-		Pipeline pipeline = Pipeline.create(options);
-
+		Pipeline pipeline = Pipeline.create(options);	
+		
 		/**
 		 * Read logs coming from Cloud Pub Sub 
 		 */
-		pipeline.apply("Read Logs Pubsub",PubsubIO.readMessages().fromTopic("projects/cloudcover-sandbox/topics/newTopic"))
+		PCollection<PubsubMessage> inputCollection = pipeline
+				.apply("Read PaloAlto Logs from Pubsub",PubsubIO.readMessages().fromSubscription((options.getReadSubscriptionName())));
+		
+		PCollection<String> netFlowCollection = pipeline
+				.apply("Read NetFlow Logs from Pubsub",PubsubIO.readStrings().fromSubscription((options.getNetflowSubscriptionName())));
 		/**
-		 * Decompress Logs  
+		 * Logs Transformation  
 		 */
-		.apply("Decompressing Logs",ParDo.of(new LogsFn()))
+		PCollection<String> stringCollection = inputCollection.apply("Raw to String",ParDo.of(new LogsFn()));	
+		//PCollection<String> stringNetFlowCollection = netFlowCollection.apply("Binary to String",ParDo.of(new ConvertBinaryToString()));	
 		/**
-		 * Window into fixed windows. The fixed window size is 5
+		 * Write to Pub Sub Topic
+		*/
+		stringCollection.apply("Write PaloAlto Logs to Elk",PubsubIO.writeStrings().to(options.getWriteTopicName()));		
+		/**
+		 * Window into fixed windows. The fixed window size for 5 minutes
 		 * minute.
 		 */
-		.apply("Applying Window",Window.into(FixedWindows.of(Duration.standardMinutes(options.getWindowSize()))))
-
+		netFlowCollection.apply("Applying Window",Window.into(FixedWindows.of(Duration.standardMinutes(options.getWindowSize()))))
+		.apply("Write to NetFlow Bucket",TextIO.write().to(options.getNetflowOutput()).withWindowedWrites().withNumShards(1));
+		
+		stringCollection.apply("Applying Window",Window.into(FixedWindows.of(Duration.standardMinutes(options.getWindowSize()))))
 		/**
 		 * Write log files to Google Cloud Storage bucket
 		 */		
-		.apply("Write Logs GCS",TextIO.write().to(options.getOutput()).
-		     withWindowedWrites().withNumShards(1));
-
+		.apply("Write to Output Bucket",TextIO.write().to(options.getOutput()).
+		   withWindowedWrites().withNumShards(1));
+		
 		pipeline.run();	
 
 
